0b3892b0a9bf0ab564810ec23d8b6cd2206702b8
Stephan Hesse <stephan@emliri.com>
2020-11-05 04:33:31 +0100

    srt dyn-lib loading entry point: support debug/release fall-thru + nullable module on import as well as using absolute paths with "__dirname-join" as recommended by pkg docs to be resolved as executable snapshot assets.

--- src/srt.js
+++ src/srt.js
@@ -1,5 +1,18 @@
-try {
-  module.exports = require('../build/Debug/srt.node');
-} catch(err) {
-  module.exports = require('../build/Release/srt.node');
+const fs = require("fs");
+const path = require("path");
+
+// NOTE: Very useful in combination with PKG executable bundler,
+// see https://github.com/vercel/pkg#config
+const dynLibPathDebug = path.join(__dirname, '../build/Debug/srt.node');
+const dynLibPathRelease = path.join(__dirname, '../build/Release/srt.node');
+const haveDebug = fs.existsSync(dynLibPathDebug);
+const haveRelease = fs.existsSync(dynLibPathRelease);
+if (haveDebug) {
+  module.exports = require(dynLibPathDebug);
+} else if (haveRelease) {
+  module.exports = require(dynLibPathRelease);
+} else {
+  module.exports = {
+    SRT: null
+  };
 }
